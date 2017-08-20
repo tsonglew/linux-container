861f35d4fb025f41f88cb098575dbf7720751c60
root <root@kasheemlew-Ubuntu14.04>
2017-08-20 15:28:54 +0800

    set Uid&Gid Mappings for kernel 4.4

--- main.go
+++ main.go
@@ -11,7 +11,23 @@ func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
+                // set UidMapping&GidMappings for kernel 4.4
+                UidMappings: []syscall.SysProcIDMap{
+                        {
+                                ContainerID: 1234,
+                                HostID:      0,
+                                Size:        1,
+                        },
+                },
+                GidMappings: []syscall.SysProcIDMap{
+                        {
+                                ContainerID: 1234,
+                                HostID:      0,
+                                Size:        1,
+                        },
+                },
 	}
+          // for kernel 3.13
 //        cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(1), Gid: uint32(1)}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
