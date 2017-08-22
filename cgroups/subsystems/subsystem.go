cce101bc734e5c86a1740dbc8a2e94cac6771b2a
kasheemlew <417879751@qq.com>
2017-08-22 19:00:18 +0800

    add ResourceConfig

--- cgroups/subsystems/subsystem.go
+++ cgroups/subsystems/subsystem.go
@@ -1,1 +1,7 @@
 package subsystems
+
+type ResourceConfig struct {
+	MemoryLimit string // memory limit
+	CpuShare    string // time-sharing timeslice weights
+	CpuSet      string // number of cpu cores
+}
